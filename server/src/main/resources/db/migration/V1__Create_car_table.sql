d30791c04a0f5399b9fcd4d6ca33a8103e23dc5e
Molina Gonzalez <juan-pedro.molina-gonzalez@capgemini.com>
2020-06-22 16:05:16 +0200

    adding flyway migrations

---
+++ server/src/main/resources/db/migration/V1__Create_car_table.sql
@@ -0,0 +1,4 @@
+create table car (
+    id int not null,
+    name varchar(125) not null
+);
---
+++ server/src/main/resources/db/migration/V2__Add_cars.sql
@@ -0,0 +1,4 @@
+insert into car (id, name) values (1, 'Ferrari');
+insert into car (id, name) values (2, 'Mercedes');
+insert into car (id, name) values (3, 'Bugatti');
+insert into car (id, name) values (4, 'AMG Gremling');
